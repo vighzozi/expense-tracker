5df5b9ac77ed28ceb3df039c7f3ed30948d71908
vighzoltan <52635889+vighzozi@users.noreply.github.com>
2025-07-03 09:07:27 +0200

    login JS
    added the login logic to JS buttons are working now.

--- static/script.js
+++ static/script.js
@@ -165,6 +165,37 @@ async function submitEdit() {
     }
   });
   
+  // login
+
+  const loginForm = document.getElementById("login-form");
+
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    });
+  
+    const result = await response.json();
+    console.log(result);
+  
+    if (response.ok) {
+      alert("Successful login! 🎉");
+      loginForm.reset();
+    } else {
+      alert("ERROR: " + result.error || "Unknown error");
+    }
+  });
 window.onload = () => {
     loadExpense();
   };
